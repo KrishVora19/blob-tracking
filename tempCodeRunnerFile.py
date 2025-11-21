d5f29f5643f7dcfba663e5b7ab7fff4f93e3672a
KrishVora19 <152849141+KrishVora19@users.noreply.github.com>
2025-11-22 10:30:42 +1100

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,3 +0,0 @@
-                    avg_color_int = tuple(int(np.clip(c, 0, 255)) for c in avg_color)
-                    # avg_color is BGR; convert to RGB hex
-                    text = "#{:02x}{:02x}{:02x}".format(avg_color_int[2], avg_color_int[1], avg_color_int[0])
